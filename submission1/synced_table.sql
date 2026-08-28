f86973b6e4befa151395ed9452e0fc372dbdae0e
brendan.byam@databricks.com <brendan.byam@databricks.com>
2026-08-28 00:44:49 +0000

    sub1

--- submission1/synced_table.sql
+++
@@ -1,4 +0,0 @@
--- Query against the synced Unity Catalog table in Lakebase Postgres
--- Source: bbyam_ts.dev_brendan_byam_streamline_telco.gold_subscriber_position
--- Synced to: databricks_postgres.synced_subscriber_position
-SELECT * FROM synced_subscriber_position LIMIT 5;
